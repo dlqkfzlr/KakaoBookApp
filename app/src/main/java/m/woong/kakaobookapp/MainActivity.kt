6ac9137130833bf66bdf63778f3cc644f015db06
dlqkfzlr <hisunwoong@gmail.com>
2021-02-08 12:56:16 +0900

    Initial commit

---
+++ app/src/main/java/m/woong/kakaobookapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package m.woong.kakaobookapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import m.woong.kakaobookapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/m/woong/kakaobookapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package m.woong.kakaobookapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
