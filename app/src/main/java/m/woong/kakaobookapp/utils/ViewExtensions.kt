78b41708747fc41a8e9181be281841e8de4c3490
dlqkfzlr <hisunwoong@gmail.com>
2021-02-09 16:27:02 +0900

    Add ViewExtensions.kt

--- app/src/main/java/m/woong/kakaobookapp/utils/ViewExtensions.kt
+++ app/src/main/java/m/woong/kakaobookapp/utils/ViewExtensions.kt
@@ -1,2 +1,21 @@
 package m.woong.kakaobookapp.utils
 
+import android.widget.ImageView
+import android.widget.ToggleButton
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import m.woong.kakaobookapp.R
+
+@BindingAdapter("imageUrl")
+fun ImageView.setUrl(url: String) {
+    Glide.with(this)
+        .load(url)
+        .error(R.drawable.ic_error_outline_24)
+        .into(this)
+}
+
+/* ToggleButton */
+@BindingAdapter("favorite")
+fun ToggleButton.setFavorite(isFavorite: Boolean) {
+    this.isChecked = isFavorite
+}
