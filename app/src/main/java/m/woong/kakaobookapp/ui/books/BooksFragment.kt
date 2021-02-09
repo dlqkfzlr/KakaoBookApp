9efcee9746f7e86a38528f4f5278dc07b485d461
dlqkfzlr <hisunwoong@gmail.com>
2021-02-09 16:26:46 +0900

    Add drawable resources to BookFragment.kt

--- app/src/main/java/m/woong/kakaobookapp/ui/books/BooksFragment.kt
+++ app/src/main/java/m/woong/kakaobookapp/ui/books/BooksFragment.kt
@@ -21,7 +21,7 @@ class BooksFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        return inflater.inflate(R.layout.books_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
