56322286675e7c5ebc73dcce2ab44bb94ab439e4
dlqkfzlr <hisunwoong@gmail.com>
2021-02-09 20:50:28 +0900

    Create a model for ui layer

---
+++ app/src/main/java/m/woong/kakaobookapp/ui/model/Book.kt
@@ -0,0 +1,15 @@
+package m.woong.kakaobookapp.ui.model
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Book(
+    val isbn: String,
+    val contents: String,
+    val datetime: String,
+    val price: Int,
+    val publisher: String,
+    val thumbnail: String,
+    val title: String,
+    val isFavorite: Boolean
+): Parcelable
