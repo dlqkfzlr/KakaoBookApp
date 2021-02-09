0391d7821c6576dd5d86f27dc325581101c6adad
dlqkfzlr <hisunwoong@gmail.com>
2021-02-09 20:53:28 +0900

    Apply viewBinding to BookDetailsFragment.kt

--- app/src/main/java/m/woong/kakaobookapp/ui/details/BookDetailsFragment.kt
+++ app/src/main/java/m/woong/kakaobookapp/ui/details/BookDetailsFragment.kt
@@ -5,8 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import dagger.hilt.android.AndroidEntryPoint
+import m.woong.kakaobookapp.R
+import m.woong.kakaobookapp.databinding.BookDetailsFragmentBinding
+import m.woong.kakaobookapp.ui.MainViewModel
+import m.woong.kakaobookapp.ui.books.BooksFragment
+import m.woong.kakaobookapp.utils.setUrl
 
-class BookDetailsFragment: Fragment() {
+@AndroidEntryPoint
+class BookDetailsFragment: Fragment(R.layout.book_details_fragment), FavoriteCallBack {
+
+    companion object {
+        fun newInstance() = BookDetailsFragment()
+    }
+
+    private val viewModel: MainViewModel by activityViewModels()
+    private var _binding: BookDetailsFragmentBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,5 +34,19 @@ class BookDetailsFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        _binding = BookDetailsFragmentBinding.bind(view)
+        with(binding) {
+            bookContents.text = "bookContents"
+            bookDatetime.text = "bookDatetime"
+            bookPrice.text = "bookPrice"
+            bookPublisher.text = "bookPublisher"
+            bookTitle.text = "bookTitle"
+            ivBookCover.setUrl("https://miro.medium.com/max/725/1*xYXHOHl5bmO8peICcGbA_w.png")
+        }
+
+    }
+
+    override fun checkFavorite(isbn: String) {
+//        viewModel.updateFavorite
     }
 }
