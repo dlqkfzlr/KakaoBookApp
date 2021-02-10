c5aa300e07867e16026172fda4014d78782b4427
dlqkfzlr <hisunwoong@gmail.com>
2021-02-10 19:12:39 +0900

    Apply AppBarConfiguration to MainActivity.kt

--- app/src/main/java/m/woong/kakaobookapp/ui/MainActivity.kt
+++ app/src/main/java/m/woong/kakaobookapp/ui/MainActivity.kt
@@ -3,30 +3,30 @@ package m.woong.kakaobookapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.navigateUp
 import dagger.hilt.android.AndroidEntryPoint
 import m.woong.kakaobookapp.R
-import m.woong.kakaobookapp.ui.books.BooksFragment
-import m.woong.kakaobookapp.ui.details.BookDetailsFragment
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        /*if (savedInstanceState == null)  {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, BookDetailsFragment.newInstance())
-                .commitNow()
-        }*/
-//        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(this, navController, appBarConfiguration)
+    }
 
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
