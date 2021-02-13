8a1d0e007db108fe6f55292b00bd70000d43d3ac
dlqkfzlr <hisunwoong@gmail.com>
2021-02-13 19:00:22 +0900

    Change ViewBinding to DataBinding inside of BookDetailsFragment.kt

--- app/src/main/java/m/woong/kakaobookapp/ui/details/BookDetailsFragment.kt
+++ app/src/main/java/m/woong/kakaobookapp/ui/details/BookDetailsFragment.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import m.woong.kakaobookapp.R
 import m.woong.kakaobookapp.databinding.BookDetailsFragmentBinding
 import m.woong.kakaobookapp.ui.MainViewModel
@@ -18,33 +21,29 @@ import m.woong.kakaobookapp.utils.setUrl
 import m.woong.kakaobookapp.utils.toDate
 
 @AndroidEntryPoint
-class BookDetailsFragment: Fragment(R.layout.book_details_fragment), FavoriteCallBack {
-
-    companion object {
-        fun newInstance() = BookDetailsFragment()
-        val TAG = BookDetailsFragment::class.java.simpleName
-    }
+class BookDetailsFragment: Fragment() {
 
     private val viewModel: MainViewModel by activityViewModels()
-    private var _binding: BookDetailsFragmentBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: BookDetailsFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = DataBindingUtil.inflate(inflater, R.layout.book_details_fragment, container, false)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = BookDetailsFragmentBinding.bind(view)
         arguments?.let { args ->
             val book: Book? = args.getParcelable("BOOK_ITEM")
+            binding.book = book
             book?.let { setViewContent(it) }
         }
-
     }
 
     @SuppressLint("SetTextI18n")
@@ -56,12 +55,8 @@ class BookDetailsFragment: Fragment(R.layout.book_details_fragment), FavoriteCal
             bookPrice.text = "책 가격: ${book.price.toString().toKoreanWon()}"
             bookPublisher.text = "출판사: ${book.publisher}"
             bookContents.setParsedHtmlText(book.contents)
+            tbBookFavorite.isChecked = book.isFavorite
         }
     }
 
-
-
-    override fun checkFavorite(isbn: String) {
-//        viewModel.updateFavorite
-    }
 }
